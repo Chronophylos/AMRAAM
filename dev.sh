3ed4a1b3b2d3c09da0a33138930a624c2e4987ff
Chronophylos <nikolai@chronophylos.com>
2020-06-21 10:51:22 +0200

    allow selecting a guest os when using dev.sh

--- dev.sh
+++ dev.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 CONTAINER="amraamtest"
+OS="$1"
+shift
 
 cargo build
-docker container run -itd --rm --name $CONTAINER amraam:v1 bash >/dev/null
+docker container run -itd --rm --name $CONTAINER "amraam:$OS" bash >/dev/null
 docker container cp target/debug/amraam "$CONTAINER:amraam" >/dev/null
 docker container exec -it "$CONTAINER" amraam "$*"
 docker container stop "$CONTAINER" >/dev/null
